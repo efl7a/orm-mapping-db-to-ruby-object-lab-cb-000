13a100678f73642041ac61cbe68eda855f061e74
Heather Christman <efl7a@yahoo.de>
2018-01-02 01:31:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,4 +68,15 @@ class Student
       Student.new_from_db(row)
     end
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade != "12"
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      Student.new_from_db(row)
+    end
+  end
+
 end
